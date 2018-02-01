cd8f9fc6efc984cbd80d89c4e9dd13c61758401b
Anders Jensen <johndoee@tidalstream.org>
2018-02-01 18:47:07 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_description():
 
 setup(
     name='deluge-client',
-    version='1.1.0',
+    version='1.2.0',
     description='Simple Deluge Client',
     long_description=read_description(),
     author='Anders Jensen',
