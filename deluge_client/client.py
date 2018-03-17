2c5d224b69a68f89eebb403ffa40b2a8afea5f4c
Anders Jensen <johndoee@tidalstream.org>
2018-03-17 12:43:16 +0100

    Errors now encoded correctly, fixing #13

--- deluge_client/client.py
+++ deluge_client/client.py
@@ -138,8 +138,9 @@ class DelugeRPCClient(object):
                 exception_type, exception_msg, _, traceback = data
             else:
                 exception_type, exception_msg, traceback = data[0]
-            exception = type(str(exception_type), (Exception, ), {})
-            exception_msg = '%s\n\n%s' % (exception_msg, traceback)
+            exception = type(str(exception_type.decode('utf-8', 'ignore')), (Exception, ), {})
+            exception_msg = '%s\n%s' % (exception_msg.decode('utf-8', 'ignore'),
+                                          traceback.decode('utf-8', 'ignore'))
             raise exception(exception_msg)
         elif msg_type == RPC_RESPONSE:
             retval = data[0]
